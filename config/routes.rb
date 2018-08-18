f917824a505c2a675cda9f0b6dae98a0cdb6d453
sc610889477 <sc610889477@gmail.com>
2018-08-18 22:32:49 +0900

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :messages , only: [:create]
+  # フォームから情報を送信して保存できるように
+  #resources :messages# resources情報を確認する
 end
