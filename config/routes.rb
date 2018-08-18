8cfb62a14d1cf09cd4a186beaab4607143bdcef1
sc610889477 <sc610889477@gmail.com>
2018-08-19 00:56:05 +0900

    create form template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :messages , only: [:create]
   # フォームから情報を送信して保存できるように
-  #resources :messages# resources情報を確認する
+  #resources :messages , only: [:create]
+  # 編集機能を実装する
+  resources :messages , except: [:index, :new]
+  # resources情報を確認する
+  #resources :messages
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
